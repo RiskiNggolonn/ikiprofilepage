1c2cc7b4a892749ea1f9700d0887ed409deb49ad
RiskyRifaldy <riskyrivaldy150515@gmail.com>
2024-05-04 19:59:08 +0700

    TusagUTS

---
+++ assets/stylesheet/app.js
@@ -0,0 +1,11 @@
+//Ini adalah Logic untuk skrol
+
+document.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".nav");
+
+  if (window.scrollY >= 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
